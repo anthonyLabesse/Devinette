9d0d5a769f4d5d9507a1e08cec2f57fcb12cd952
Anthony Labesse <labesseanthony63@gmail.com>
2018-09-13 20:24:00 +0200

    Ajout de la derniere condition if

--- js/devinette.js
+++ js/devinette.js
@@ -14,24 +14,26 @@ var solution = Math.floor(Math.random() * 100) + 1;
 //console.log("(La solution est " + solution + ")");
 
 // TODO : complétez le programme
-     // Le jeu ne permet que six tentatives pour réussir la devinette
-var essai=1;
-while(essai => 6){
+     
+
+for(var essai = 1; essai <= 7; essai++){ // Le jeu ne permet que six tentatives pour réussir la devinette
     var reponse = prompt("Entrez un nombre compris entre 1 et 100 :" );
     if (reponse > solution){
-            reponse = prompt(reponse + " est trop grand, veuillez saisir un autre nombre");
+            reponse = console.log(reponse + " est trop grand, veuillez saisir un autre nombre");
         }
         else if(reponse < solution){
-            reponse = prompt(reponse + " est trop petit, veuillez saisir un autre nombre");
+            reponse = console.log(reponse + " est trop petit, veuillez saisir un autre nombre");
         }
         else{
-            reponse = prompt("Bravo, La solution est " + solution + "et tu as deviné en" + i + "essais");  
+            reponse = console.log("Bravo, La solution est " + solution + " et tu as deviné en" + essai + " essais ");
+            
         }
-    essai++;
 }
 
-if ((essai >=6 && (reponse !== solution)){
-            reponse = prompt("Perdu, La solution était " + solution + "Essaie encore !);
+essai++;
+
+if ((essai >=6) && (reponse !== solution)){
+            reponse = prompt("Perdu, La solution était "  + solution +  "\nEssaie encore !");
     }
     
 
