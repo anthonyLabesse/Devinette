c73c33bf6b49c27576edfe9898ca383c6d5afd2f
Anthony Labesse <labesseanthony63@gmail.com>
2018-09-13 17:54:10 +0200

    Agencement des boucles

--- js/devinette.js
+++ js/devinette.js
@@ -14,21 +14,27 @@ var solution = Math.floor(Math.random() * 100) + 1;
 //console.log("(La solution est " + solution + ")");
 
 // TODO : complétez le programme
-    for(var i = 1; i<=7; i++){ // Le jeu ne permet que six tentatives pour réussir la devinette
-        var reponse = prompt("Entrez un nombre compris entre 1 et 100 :" );{
-            if (reponse > solution){
-              reponse = prompt(reponse + " est trop grand, veuillez saisir un autre nombre");
-              }
-              else if(reponse < solution){
-              reponse = prompt(reponse + " est trop petit, veuillez saisir un autre nombre");
-              }
-              else{
-              console.log("Bravo, La solution est " + solution + "et tu as deviné en" + i + "essais");  
-              }
-              
-              console.log("Dommage, La solution était " + solution + "Essaie encore !");
-     }
-       
+     // Le jeu ne permet que six tentatives pour réussir la devinette
+var essai=1;
+while(essai => 6){
+    var reponse = prompt("Entrez un nombre compris entre 1 et 100 :" );
+    if (reponse > solution){
+            reponse = prompt(reponse + " est trop grand, veuillez saisir un autre nombre");
+        }
+        else if(reponse < solution){
+            reponse = prompt(reponse + " est trop petit, veuillez saisir un autre nombre");
+        }
+        else{
+            reponse = prompt("Bravo, La solution est " + solution + "et tu as deviné en" + i + "essais");  
+        }
+    essai++;
+}
+
+if ((essai >=6 && (reponse !== solution)){
+            reponse = prompt("Perdu, La solution était " + solution + "Essaie encore !);
+    }
+    
+
 
     
 
